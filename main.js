9ca3997e108f1e55a44f45fa70b71e09eebe9889
Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-02-27 00:48:23 +0200

    photos and some js

---
+++ main.js
@@ -0,0 +1,52 @@
+// Масив об'єктів із зображеннями та описом
+const images = [
+	{
+		src: './Photos/evolve-cyberpart-gold-h-evcp-ghr410n305-16s480h1tbk-black.png',
+		alt: 'Товар 1',
+		description: 'Тепла кофта чоловіча, базова'
+	},
+
+	{
+		src: './Photos/evolve-optipart-silver-3h-evop-s3hi114fn306-32s500h1tbk-black.png',
+		alt: 'Товар 2',
+		description: 'Опис зображення 2'
+	},
+
+	{
+		src: './Photos/evolve-starterpart-bronze-b-evsp-bbr560g-16s500hbk-black.png',
+		alt: 'Товар 3',
+		description: 'Опис зображення 3'
+	},
+
+	{
+		src: './Photos/t-computer-gaming-gtx1660s-base-tcgb-10100n1660s-16s480bk-black.png',
+		alt: 'Товар 4',
+		description: 'Опис зображення 4'
+	},
+
+	{
+		src: './Photos/Photo-3.jpeg',
+		alt: 'Товар 3',
+		description: 'Опис зображення 3'
+	},
+
+	{
+		src: './Photos/Photo-3.jpeg',
+		alt: 'Товар 3',
+		description: 'Опис зображення 3'
+	},
+
+	{
+		src: './Photos/Photo-3.jpeg',
+		alt: 'Товар 3',
+		description: 'Опис зображення 3'
+	}
+];
+
+const imagesContainer = document.getElementById('images-container');
+const showImagesBtn = document.getElementById('show-images-btn');
+
+function addImageToContainer()
+{
+    
+}
