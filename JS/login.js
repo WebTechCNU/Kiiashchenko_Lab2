327905985e2c4027fe90cadf8ee4487d0634b359
Kiiashchenko_Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-05-13 16:49:48 +0300

    login sign in

--- JS/login.js
+++ JS/login.js
@@ -4,24 +4,57 @@ function login() {
 }
 
 function signin() {
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let password_confirm = document.getElementById("password_confirm").value;
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let password_confirm = document.getElementById("password_confirm");
 
-  if (password != password_confirm) {
-    alert("Паролі не співпадають!");
+  if (password.value.length < 8) {
+    alert("Пароль має бути не менше 8 символів!");
+    password.value = "";
+    password_confirm.value = "";
     return;
   }
+
+  if (email.value == "") {
+    alert("Введіть email!");
+    return;
+  }
+
+  if (password.value != password_confirm.value) {
+    alert("Паролі не співпадають!");
+    password.value = "";
+    password_confirm.value = "";
+    return;
+  }
+
 
   let email_local = localStorage.getItem("email");
   let password_local = localStorage.getItem("password");
 
-  if (email == email_local && password == password_local) {
+  if (email.value == email_local && password.value == password_local) {
     alert("Ви вже зареєстровані!");
   } else {
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", password);
+    localStorage.setItem("email", email.value);
+    localStorage.setItem("password", password.value);
   }
+
+  login_init();
 }
-// let email = document.getElementById("email");
-// let password = document.getElementById("password");
+
+function login() {
+  let email_local = localStorage.getItem("email");
+  let password_local = localStorage.getItem("password");
+
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+
+  if (email.value == email_local && password.value == password_local) {
+    alert("Ви успішно увійшли!");
+    window.location.href = "index.html";
+    return;
+  }
+
+  alert("Невірний логін або пароль!");
+}
+
+
